a949a290992d7c7c2f8a886190daa254ba67e67c
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-12 10:27:22 +0530

    feat: create athletes, coaches, genders, get endpoint

--- api/routes/tokyo_olympics.py
+++ api/routes/tokyo_olympics.py
@@ -5,11 +5,25 @@ from typing import Union
 router = APIRouter()
 
 athletes = pd.read_excel("datasets/2021/Athletes.xlsx")
+coaches = pd.read_excel("datasets/2021/Coaches.xlsx")
+genders = pd.read_excel("datasets/2021/EntriesGender.xlsx")
 
 # Root endpoint for the Tokyo Olympics 2021 dataset
 @router.get("/")
 def root():
     return {"status": "ok", "dataset": "Tokyo Olympics 2021"}
+
+@router.get("/get/{data}")
+def get_data(data: str):
+    if data == "athletes":
+        return {"countries": sorted(athletes["NOC"].unique().tolist()), "sports": sorted(athletes["Discipline"].unique().tolist())}
+    elif data == "coaches":
+        return {"countries": sorted(coaches["NOC"].unique().tolist()), "sports": sorted(coaches["Discipline"].unique().tolist())}
+    elif data == "genders":
+        return {"genders": ["Male", "Female"], "sports": sorted(genders["Discipline"].unique().tolist())}
+    else:
+        return {}
+
 
 @router.get("/athletes")
 def get_athletes(country: Union[str, None] = None, sport: Union[str, None] = None):
@@ -29,19 +43,43 @@ def get_athletes(country: Union[str, None] = None, sport: Union[str, None] = Non
     else:
         total = len(athletes)
 
-    return {"athletes": total}
+    return {"total": total}
 
 
 @router.get("/coaches")
-def get_coaches():
-    # This is just a placeholder. You can replace it with actual data or logic.
-    return {"coaches": ["Coach 1", "Coach 2", "Coach 3"]}
+def get_coaches(country: Union[str, None] = None, sport: Union[str, None] = None):
+    conditions = []
+
+    if country:
+        conditions.append(coaches["NOC"] == country)
+    if sport:
+        conditions.append(coaches["Discipline"] == sport)
+
+    if conditions:
+        filtered = coaches[conditions[0]]
+        for condition in conditions[1:]:
+            filtered = filtered[condition]
+        
+        total = len(filtered)
+    else:
+        total = len(coaches)
+
+    return {"total": total}
 
 
 @router.get("/genders")
-def get_genders():
-    # This is just a placeholder. You can replace it with actual data or logic.
-    return {"genders": ["Male", "Female"]}
+def get_genders(sport: Union[str, None] = None, gender: Union[str, None] = None):
+    filtered = genders
+    if sport:
+        filtered = filtered[genders["Discipline"] == sport]
+
+    total = 0
+    if gender and gender.lower() in ["male", "female"]:
+        total += sum(filtered[genders[gender.capitalize()] != 0][gender.capitalize()].tolist())
+    else:
+        total += sum(filtered["Total"].tolist())
+
+    return {"total": total}
 
 
 @router.get("/medals")
