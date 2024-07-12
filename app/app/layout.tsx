b42e62d1fd1b354a55c64f56d4381d8da81a22e0
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-12 13:01:14 +0530

    chore: use default custom cursor on root layout

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} cursor-default`}>
         <Main fontClass={pressStart2P.className} />
         <Footer fontClass={pressStart2P.className} />
       </body>
